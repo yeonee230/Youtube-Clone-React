24ba16b3d8a622164cf2d6d1012f6c690478d53b
SUYEON KIM <yeonee230@gmail.com>
2023-04-29 18:11:44 +0900

    add axios

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -10,9 +12,8 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(["videos", keyword], async () => {
-    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios.get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.data.items);
   });
   return (
     <>
@@ -21,9 +22,7 @@ export default function Videos() {
       {error && <h1>ERROR!!!</h1>}
       {videos && (
         <ul>
-          {videos.map((video) => (
-            <li>{video.snippet.title}</li>
-          ))}
+          {videos.map((video) => <VideoCard key={video.id} video={video} />)}
         </ul>
       )}
     </>
