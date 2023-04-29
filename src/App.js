55eab2ac2dd62b3ca322691f2103328cd79b372a
SUYEON KIM <yeonee230@gmail.com>
2023-04-29 18:00:57 +0900

    make video list : using mock data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 
 export default function App() {
+  // Create a client
+  const queryClient = new QueryClient()
  
   return (
     <div>
     <SearchBar />
+    {/* // Provide the client to your App */}
+    <QueryClientProvider client={queryClient}>
     <Outlet />
+    </QueryClientProvider>
   </div>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 export default function Videos() {
   const { keyword } = useParams();
+  // Access the client
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
   return (
-    <div>
-      <h1>Videos{keyword ?  `🔍 : ${keyword}` : '🔥'}</h1>
-      {/* 만약 prams에서 받은 키워드가 있다면  "Videos 🔍 : keyword" : "Videos🔥" */}
-    </div>
+    <>
+      <h1>Videos{keyword ? `🔍 : ${keyword}` : "🔥"}</h1>
+      {isLoading && <p>로딩중...</p>}
+      {error && <h1>ERROR!!!</h1>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <li>{video.snippet.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
