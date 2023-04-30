19a8b465394edbc99068b406740d56727596e2d8
SUYEON KIM <yeonee230@gmail.com>
2023-05-01 01:25:55 +0900

    prepare deploy:

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react";
-import FakeYoutubeClient from "../api/fakeYoutubeClient";
+//import FakeYoutubeClient from "../api/fakeYoutubeClient";
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtubeClinet";
 
 export const YoutubApiContext = createContext();
 
-const client = new FakeYoutubeClient();
-//const client = new YoutubeClient();
+//const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubApiProvider({ children }) {
